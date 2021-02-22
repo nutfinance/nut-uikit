c00e46caf142cacbfde7acd50bac5d8fb58130f7
nutfinance <nutfinance@protonmail.com>
2021-02-22 20:34:57 +0800

    init

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -120,7 +120,7 @@ export const socials = [
     items: [
       {
         label: "English",
-        href: "https://t.me/nutfinance",
+        href: "https://t.me/nutdefi",
       },
       // {
       //   label: "Bahasa Indonesia",
